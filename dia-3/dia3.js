9c18bb458642184dfbe56eb32e4fbf554dbaafa7
Yasmin Nogueira <nogueira9141@gmail.com>
2025-02-05 18:09:57 -0400

    minha primeira interface de usercom variaveis e operadores aritimeticos

--- dia-3/dia3.js
+++ dia-3/dia3.js
@@ -1,14 +1,22 @@
 //let e const
 
-let meuNome = "Yasmin Nogueira"
-console.log(meuNome)
-meuNome = "yas"
-console.log(meuNome)
+let nome = ""
+let idade = 0
+let altura = 0
+let peso = 0
 
-console.log("===constante===")
+//solicitando informacao ao usuario e atribuindo os valores as variaveis
+nome = prompt("Digite seu nome: ")
+idade = parseInt(prompt("Digite sua idade: "))
+altura = parseFloat(prompt("Digite sua altura: "))
+peso = parseInt(prompt("Digite seu peso: "))
 
+//calculando o ano que a pessoa nasceu e o IMC
+let yearNasc = 0
+yearNasc = 2025 - idade
 
-const a = "Yasmin Nogueira"
-console.log(a)
-a = "yas"
-console.log(a)
+let imc = 0
+imc = peso / (altura*altura)
+
+//Exibindo as informacoes no console
+console.log ("Olá " + nome + ", você tem " + idade + " anos, nasceu em " + yearNasc + ", tem " + altura + " de altura, pesa " + peso + "KG e seu IMC é " + imc + "KG/m²")
