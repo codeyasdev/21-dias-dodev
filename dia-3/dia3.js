c29ab7a910d4261c9b555eba102199a67f2d76b5
YASMIN NOGUEIRA <nogueira9141@gmail.com>
2025-02-05 17:00:24 -0400

    aprendendo sobre variaveis e constantes

---
+++ dia-3/dia3.js
@@ -0,0 +1,14 @@
+//let e const
+
+let meuNome = "Yasmin Nogueira"
+console.log(meuNome)
+meuNome = "yas"
+console.log(meuNome)
+
+console.log("===constante===")
+
+
+const a = "Yasmin Nogueira"
+console.log(a)
+a = "yas"
+console.log(a)
