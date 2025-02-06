c11168e16656a5b4edfcc05e6bb33ec3e2092fa3
Yasmin Nogueira <nogueira9141@gmail.com>
2025-02-06 16:09:54 -0400

    meu primeiro contato com switch case

---
+++ dia-5/dia-5.js
@@ -0,0 +1,14 @@
+//variavel
+let idade = Number(prompt("insira a sua idade: "))
+
+switch (idade){
+    case 20:
+        console.log("Você tem 20 anos")
+        break;
+    case 25:
+        console.log("Você tem 25 anos")
+        break;
+    default:
+        console.log("DEFAULT")
+        break;
+}
