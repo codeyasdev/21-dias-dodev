1badb8b5ce329abe751506630ca3c7b197ea93e6
Yasmin Nogueira <nogueira9141@gmail.com>
2025-02-06 13:54:34 -0400

    Aprendendo meu primeiro condicional

---
+++ dia-4/dia-4.js
@@ -0,0 +1,11 @@
+let idade = Number(prompt("insira a sua idade: "))
+//entendendo IF e ELSE
+// == igual | != | <> | <= e >=
+// === valor, igual, tipo
+
+if(idade <= 10){
+    console.log("Você tem menos ou tem 10 anos")
+} else{
+        console.log("Você tem mais que 10 anos")
+    }
+
